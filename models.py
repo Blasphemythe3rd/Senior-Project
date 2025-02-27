df8b45edd14ba5d92a3860d67c518a54435b3bb0
Nick Lunt <luntni22@students.rowan.edu>
2025-02-26 20:40:15 -0500

    Start models.py

--- models.py
+++ models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# Computed objects cache computations that were already performed
-# A computation that has already been performed will not be performed again 
-# class Computed(models.Model):  
-#     input = models.IntegerField()
-#     output = models.IntegerField()
-#     time_computed = models.DateTimeField(null=True)
-
-#     def __str__(self):
-#         return(f"{self.input} -> {self.output}")
-
 class Given_Stimuli(models.Model):
     given_stimuli = models.TextField()
     options_stimuli = models.TextField()
