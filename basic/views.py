ffc13ea3a112c47351f695a315634b6b3bf5875c
Nick Lunt <luntni22@students.rowan.edu>
2025-02-26 20:38:50 -0500

    Cleaned up starter project, started models

---
+++ basic/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import Http404
+from .models import Computed
+from django.utils import timezone
+
+# Create your views here.
+
+# Computation function
+def compute(request, value):
+    try:
+        input = int(value)
+        precomputed = Computed.objects.filter(input=input)
+        if precomputed.count() == 0:  # The answer for this input has not been computed
+            # Compute the answer
+            answer = input * input
+            time_computed = timezone.now()
+            # Save it into the database
+            computed = Computed(
+                input=input, 
+                output=answer,
+                time_computed=time_computed
+            )
+            computed.save() # Store it into the database
+        else: 
+            computed = precomputed[0] 
+        
+        return render (
+            request,
+            "basic/compute.html",
+            {
+                'input': input,
+                'output': computed.output,
+                'time_computed': computed.time_computed.strftime("%m-%d-%Y %H:%M:%S UTC")
+            }
+        )
+    except:
+        raise Http404(f"Invalid input: {value}")
+
+
+    
---
+++ models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+# Computed objects cache computations that were already performed
+# A computation that has already been performed will not be performed again 
+# class Computed(models.Model):  
+#     input = models.IntegerField()
+#     output = models.IntegerField()
+#     time_computed = models.DateTimeField(null=True)
+
+#     def __str__(self):
+#         return(f"{self.input} -> {self.output}")
+
+class Given_Stimuli(models.Model):
+    given_stimuli = models.TextField()
+    options_stimuli = models.TextField()
+    enum_type = models.TextField()
+
+class Stimuli_Response(models.Model):
+	enum_type = models.TextField()
+	response = models.TextField()
+	repsonse_time = models.FloatField()
+	response_per_click = models.IntegerField()
+
+class Test(models.Model):
+    test_id = models.TextField()
+    stimuli = models.OneToMany(Given_Stimuli)
+    responses = models.OneToMany(Stimuli_Response)
+    # patient = models.ForeignKey(Patient)
+    time_started = models.DateTimeField(auto_now_add = True)
+    time_ended = models.DateTimeField(auto_now_add = True)
+    status = models.IntegerField(default = 0)  # 0 = not taken
+    patient_age = models.IntegerField()
+
+class Doctor(User):
+	assigned_tests = models.ArrayField(models. TextField())
+	# Implements Django’s User Class
+
+class Notification(models.Model):
+	status = models.IntegerField
+	message = models.TextField()
+	users = models.ManyToManyField(Doctor)
+	Model.objects.filter(assigned_tests__contains=['doctors_username'])
