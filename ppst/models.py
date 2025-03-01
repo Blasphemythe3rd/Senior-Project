af9b8e5dc858b527cd48609f8885330839d66318
Nick Lunt <luntni22@students.rowan.edu>
2025-03-01 16:46:37 -0500

    updated models

--- ppst/models.py
+++ ppst/models.py
@@ -3,33 +3,34 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Given_Stimuli(models.Model):
-    given_stimuli = models.TextField()
-    options_stimuli = models.TextField()
-    enum_type = models.TextField()
-
-class Stimuli_Response(models.Model):
-	enum_type = models.TextField()
-	response = models.TextField()
-	repsonse_time = models.FloatField()
-	response_per_click = models.IntegerField()
-
 class Test(models.Model):
     test_id = models.TextField()
-    stimuli = models.OneToMany(Given_Stimuli)
-    responses = models.OneToMany(Stimuli_Response)
+    # stimuli = models.ForeignKey(Given_Stimuli)
+    # responses = models.ForeignKey(Stimuli_Response)
     # patient = models.ForeignKey(Patient)
     time_started = models.DateTimeField(auto_now_add = True)
     time_ended = models.DateTimeField()
     status = models.IntegerField(default = 0)  # 0 = not taken
     patient_age = models.IntegerField()
 
-class Notification(models.Model):
-    status = models.IntegerField
-    message = models.TextField()
-    # users = models.ManyToManyField(Doctor)
-    # Model.objects.filter(assigned_tests__contains=['doctors_username'])
+class Given_Stimuli(models.Model):
+    given_stimuli = models.TextField()
+    options_stimuli = models.TextField()
+    enum_type = models.TextField()
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+class Stimuli_Response(models.Model):
+    enum_type = models.TextField()
+    response = models.TextField()
+    repsonse_time = models.FloatField()
+    response_per_click = models.IntegerField()
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
 class Doctor(User): 	# Implements Django’s User Class
     assigned_tests = models.ArrayField(Test)
-    notifications = models.OneToMany(Notification)
+
+class Notification(models.Model):
+    status = models.IntegerField
+    message = models.TextField()
+    users = models.ManyToManyField(Doctor)
+    # Model.objects.filter(assigned_tests__contains=['doctors_username'])
