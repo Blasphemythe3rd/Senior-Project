6e32a936233368cbeb868a63bfb0c843c4a6743c
price360 <adschneck125@gmail.com>
2025-03-24 16:25:13 -0400

    *UNFINISHED* imported all stimuli from backend, need to finish the correct display of it.
    *FINISHED* Buttons now flash briefly.

--- PPST/views.py
+++ PPST/views.py
@@ -2,13 +2,12 @@ import random
 from django.shortcuts import render
 from .models import Given_Stimuli
 
+
 def practiceTest(request):
     return render(request,'practiceTest.html')
 
 def testScreen(request):
-    stimuli_0 = Given_Stimuli.objects.first()  # Get the first object (you can change this query)
-    
-    # If no object exists, provide an empty string or handle it differently
-    given_stimuli_str = stimuli_0.given_stimuli if stimuli_0 else ""
+    stimuli_objects = Given_Stimuli.objects.all()
+    stimuli_list = [stimulus.given_stimuli for stimulus in stimuli_objects]
 
-    return render(request, 'testScreen.html', {'given_stimuli_str': given_stimuli_str})
+    return render(request, 'testScreen.html', {'stimuli_list': stimuli_list})
