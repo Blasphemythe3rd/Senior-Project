1c63e156bbca86b2c680b909ae6d922a1de6cad2
bvfr <67278922+bvfr@users.noreply.github.com>
2025-03-25 10:41:59 -0400

    Doctor Login URL

--- PPST/urls.py
+++ PPST/urls.py
@@ -5,7 +5,7 @@ app_name="PPST"
 
 urlpatterns = [
     path("test/", views.test, name="test"),
-    
+    path('login/', views.doctor_login, name='doctor_login'),
     path("testInfo/", views.testInfo, name="testInfo"),
     path("download_test/<str:test_id>/", views.download_test, name="download_test"),
 ]
