b4b8011cf57ea55cf1fd27e81b9f830dea31e9d1
price360 <adschneck125@gmail.com>
2025-03-25 17:23:06 -0400

    fix views.py anf fixture.py

--- PPST/views.py
+++ PPST/views.py
@@ -1,15 +1,3 @@
-import random
-from django.shortcuts import render
-from .models import Given_Stimuli
-
-def practiceTest(request):
-    return render(request,'practiceTest.html')
-
-def testScreen(request):
-    stimuli_objects = Given_Stimuli.objects.all()
-    stimuli_list = [stimulus.given_stimuli for stimulus in stimuli_objects]
-
-    return render(request, 'testScreen.html', {'stimuli_list': stimuli_list})
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_GET, require_POST
@@ -23,6 +11,7 @@ import json
 import csv
 
 # Create your views here.
+
 
 import random
 from django.shortcuts import render
