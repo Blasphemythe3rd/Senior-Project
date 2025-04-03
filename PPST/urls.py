bdd76f0b2366406f2f110990c30c0738d0c8b9b1
PeterShelinsky1 <peter109595@gmail.com>
2025-04-03 18:46:10 -0400

    Add average statistics and age distribution to admin page, and added emails to add doctor

--- PPST/urls.py
+++ PPST/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("admin/", views.admin_page, name="admin"),  
     path("admin/list_doctors/", views.list_doctors, name="list_doctors"),
     path("admin/add_doctor/", views.add_doctor, name="add_doctor"),
+    path('admin/overall_statistics/', views.overall_average_statistics, name='overall_statistics'),    
     path('admin/doctor_tests/<int:doctor_id>/', views.doctor_tests, name='doctor_tests'),
     path('admin/test_details/<int:test_id>/', views.fetch_test_details, name='fetch_test_details'),
     path('admin/login/', views.admin_login, name='admin_login'),
